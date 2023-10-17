dcb8851a8512638c570f99da995fa813153b55d0
Maria Eduarda Rodrigues <maria.rodrigues18@estudante.ifms.edu.br>
2023-10-17 10:14:50 -0400

    contato

---
+++ src/componentes/contato/Contato.tsx
@@ -0,0 +1,28 @@
+import './Contato.css'
+import { useState } from 'react';
+
+function Contato(){
+
+    const [textoContato, setTextoContato]=useState("")
+
+    function TrataTextoContato(e:React.ChangeEvent<HTMLInputElement>){
+        setTextoContato(e.target.value)
+    }
+
+    return(
+        <div className="contato-main">
+        <h1>Contato</h1>
+        <form className='formContato'>
+            <input className='input-contato' type="text" placeholder='Nome Completo' onChange={TrataTextoContato}/>
+            <input className='input-contato' type="email" placeholder='E-mail' onChange={TrataTextoContato}/>
+            <input className='input-contato' type="tel" placeholder='Telefone' maxLength={11} onChange={TrataTextoContato}/>
+            <input className='input-contato' type="text" placeholder='Endereço' onChange={TrataTextoContato}/>
+            <input className='button-enviar' type="submit" value="Enviar" onChange={TrataTextoContato}/>
+            {textoContato && <p>Resultados para: {textoContato}</p>}
+        </form>
+        </div>
+    )
+}
+
+
+export default Contato
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Header from './componentes/header/Header'
 import Footer from './componentes/footer/Footer'
 import Main from './componentes/main/Main'
+import Contato from './componentes/contato/Contato'
 import './main.css'
 
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
@@ -21,16 +22,7 @@ const router = createBrowserRouter([
     element: 
       <>  
         <Header/>
-        <div className="contato-main">
-        <h1>Contato</h1>
-        <form className='formContato'>
-            <input className='input-contato' type="text" placeholder='Nome Completo'/>
-            <input className='input-contato' type="email" placeholder='E-mail'/>
-            <input className='input-contato' type="tel" placeholder='Telefone' maxLength={11}/>
-            <input className='input-contato' type="text" placeholder='Endereço'/>
-            <input className='button-enviar' type="submit" value="Enviar"/>
-        </form>
-        </div>
+        <Contato/>
         <Footer/>
       </>
   }
